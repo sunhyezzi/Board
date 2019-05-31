f19e4ca6e0afdab5caf37f66820f65ca2a182f78
sunhyezzi <sunhye0710@gmail.com>
2019-06-01 00:32:03 +0900

    sunhye

--- crudprojrct/settings.py
+++ crudprojrct/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','^m70l=_r$xzpgdncfgou9a_ztdp%m3i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG',True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
